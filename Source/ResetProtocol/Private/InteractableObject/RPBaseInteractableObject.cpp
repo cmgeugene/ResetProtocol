fba5c19c24b5dac2aaacbb7a86681d8e50061042
JeongJaeHoon <sotakqmf01@naver.com>
2025-08-12 00:47:01 +0900

    BaseInteractObject 누락된 것 submit
    [git-p4: depot-paths = "//depot/": change = 36]

---
+++ Source/ResetProtocol/Private/InteractableObject/RPBaseInteractableObject.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "InteractableObject/RPBaseInteractableObject.h"
+#include "Components/BoxComponent.h"
+
+ARPBaseInteractableObject::ARPBaseInteractableObject()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComp"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+	StaticMeshComp->SetIsReplicated(true);
+
+	SkeletalMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("USkeletalMeshComp"));
+	SkeletalMeshComp->SetupAttachment(SceneRoot);
+	SkeletalMeshComp->SetIsReplicated(true);
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollsionBox"));
+	CollisionBox->SetupAttachment(RootComponent);
+}
+
+void ARPBaseInteractableObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (StaticMeshComp && StaticMeshComp->GetStaticMesh())
+	{
+		SkeletalMeshComp->SetVisibility(false);
+		ActiveMesh = StaticMeshComp;
+	}
+	else if (SkeletalMeshComp && SkeletalMeshComp->GetSkeletalMeshAsset())
+	{
+		StaticMeshComp->SetVisibility(false);
+		ActiveMesh = SkeletalMeshComp;
+	}
+
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ARPBaseInteractableObject::OnObjectOverlap);
+}
+
+void ARPBaseInteractableObject::Highlight()
+{
+	// SetRenderCustomDepth()/SetCustomDepthStencilValue()는 PrimitiveComponent의 함수
+	if (ActiveMesh)
+	{
+		if (UStaticMeshComponent* ActiveStaticMesh = Cast<UStaticMeshComponent>(ActiveMesh))
+		{
+			ActiveStaticMesh->SetRenderCustomDepth(true);
+			ActiveStaticMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+		}
+		else if (USkeletalMeshComponent* ActiveSkeletalMesh = Cast<USkeletalMeshComponent>(ActiveMesh))
+		{
+			ActiveStaticMesh->SetRenderCustomDepth(true);
+			ActiveStaticMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+		}
+	}
+}
+
+void ARPBaseInteractableObject::UnHighlight()
+{
+	if (ActiveMesh)
+	{
+		if (UStaticMeshComponent* ActiveStaticMesh = Cast<UStaticMeshComponent>(ActiveMesh))
+		{
+			ActiveStaticMesh->SetRenderCustomDepth(false);
+		}
+		else if (USkeletalMeshComponent* ActiveSkeletalMesh = Cast<USkeletalMeshComponent>(ActiveMesh))
+		{
+			ActiveStaticMesh->SetRenderCustomDepth(false);
+		}
+	}
+}
+
+void ARPBaseInteractableObject::OnObjectOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
---
+++ Source/ResetProtocol/Public/InteractableObject/RPBaseInteractableObject.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RPBaseInteractableObject.generated.h"
+
+#define CUSTOM_DEPTH_RED 250
+class UBoxComponent;
+
+UCLASS()
+class RESETPROTOCOL_API ARPBaseInteractableObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ARPBaseInteractableObject();
+	
+	void Highlight();
+	void UnHighlight();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> SceneRoot;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> StaticMeshComp;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComp;
+
+protected:
+	// 현재 사용하고 있는 Mesh
+	TObjectPtr<UMeshComponent> ActiveMesh;
+
+// 테스트용
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UBoxComponent> CollisionBox;
+
+	UFUNCTION()
+	virtual void OnObjectOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
